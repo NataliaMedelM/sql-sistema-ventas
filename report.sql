1624c059a4e03bfe32bb02de04be0cbb9a5abd0f
Mannu <manueld.labradorr@gmail.com>
2026-04-22 15:47:22 -0400

    Create report.sql

---
+++ report.sql
@@ -0,0 +1,81 @@
+-- =====================================
+-- REPORTE SQL - CHALLENGER SISTEMA VENTAS
+-- =====================================
+
+-- 1. Mostrar todos los clientes registrados
+
+
+-- 2. Mostrar todos los productos disponibles
+
+
+-- 3. Mostrar todas las ventas realizadas
+
+
+-- 4. Mostrar solo nombre y email de clientes
+
+
+-- 5. Mostrar solo nombre y precio de productos
+
+
+-- 6. Productos con precio mayor a 50000
+
+
+-- 7. Ventas realizadas el 2026-04-02
+
+
+-- 8. Productos ordenados de mayor a menor precio
+
+
+-- 9. Clientes ordenados por nombre
+
+
+-- 10. Detalles de venta con cantidad >= 2
+
+
+-- 11. Total de clientes
+
+
+-- 12. Total de productos
+
+
+-- 13. Total de ventas
+
+
+-- 14. Precio promedio de productos
+
+
+-- 15. Suma total de precios de productos
+
+
+-- 16. Mostrar venta + nombre del cliente + fecha
+
+
+-- 17. Mostrar detalle de ventas con id_venta + nombre producto + cantidad
+
+
+-- 18. Mostrar nombre del cliente + id de venta + fecha
+
+
+-- 19. Mostrar nombre del producto + cantidad vendida + id de venta
+
+
+-- 20. Mostrar cuántas ventas ha realizado cada cliente
+
+
+-- 21. Mostrar solo los clientes con más de una venta
+
+
+-- 22. Mostrar cuántas veces aparece cada producto en detalle_venta
+
+
+-- 23. Mostrar solo los productos que aparecen más de una vez
+
+
+-- 24. Mostrar las ventas que tienen más de un producto asociado
+
+
+-- 25. Mostrar clientes cuya suma total de unidades compradas sea mayor a 2
+
+
+-- 26. Consulta trampa que no devuelva resultados
+-- Explicar por qué el resultado vacío es correcto
